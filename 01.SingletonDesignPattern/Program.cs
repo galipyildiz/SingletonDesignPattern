b198a34d4af7eb2651844b94c5312a57a630161a
galip975 <70694743+galip975@users.noreply.github.com>
2021-02-02 03:30:46 +0300

    Add project files.

---
+++ 01.SingletonDesignPattern/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _01.SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton tekil demek ilgili nesneden 1 tane oluşmasını istiyorsak singleton design patern i kullanabiliriz.(Creational design Patern)
+            Singleton singleton1 = Singleton.GetSingleton();//
+            Singleton singleton2 = Singleton.GetSingleton();//2 nesneninde referansı aynı! Yapılan tüm değişiklikler tek bir nesne altında oluyor.
+            singleton1.Ad = "singlenton1";
+            Console.WriteLine(singleton2.Ad);
+            singleton2.Ad = "singleton2";
+            Console.WriteLine(singleton1.Ad);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Singleton singleton3 = Singleton.GetSingleton(); //yukarda 1 kere oluştuğu için burda oluşmadı.
+            }
+
+            //Multithreading ile denendi diğer bir threadde de yeni singleton oluşmadı.
+            Thread thread = new Thread(SingletonOlustur);
+            thread.Start();
+
+            Console.ReadKey();
+        }
+        static void SingletonOlustur()
+        {
+            Singleton singleton3 = Singleton.GetSingleton();
+            singleton3.Ad = "singleton3";
+            Console.WriteLine(singleton3.Ad);
+        }
+    }
+}
---
+++ 01.SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.SingletonDesignPattern
+{
+    public class Singleton
+    {
+        //Nesneyi kendi içinde oluşturuyoruz.
+        //Nesneyi oluşturmadan metodunu kullanabilmek için metodu static yapıyoruz.
+        private static Singleton singleton; // = new Singleton(); ////everloading kullanılmasa bile 1 tane oluşturuluyor.
+        private static int sayi = 0;//Kaç kere oluşturmayı denediğimizi yazdırıyoruz.
+        private static readonly object padlock = new object(); //multithreading işlemi için böyle bir nesne ürettik.
+        public string Ad { get; set; }
+        private Singleton()
+        {
+            Console.WriteLine("Ben oluştum.");
+        }
+        public static Singleton GetSingleton()//static metodlar içinde static dedişkenler kullanmaya izin verir.
+        {
+            //nesneye ihtiyaç duyulması demek GetSingleton() metodunun çağırılması demektir.
+            if (singleton == null) //sadece null olduğu durumda multithreadi kitliyoruz.
+            {
+                lock (padlock) //multithread işlemlerdeki açığı önlemek için
+                {
+                    if (singleton == null)
+                    {
+                        singleton = new Singleton();//lazy loading
+                    }
+                }
+            }
+            //2'li null kontrol kalıbına Double Checked locking kalıbı denir. Bu kalıp sayesinde hem lazy loading hem thread safe çalıştık hemde maximum performans yapmış olduk.
+            sayi++;
+            Console.WriteLine(sayi + " kere Oluşturulmayı denendim.");
+            return singleton;
+        }
+    }
+}
